79910c4e2dd03a31856bb11402b6c33902c970b9
MUTHURAJA1997 <37202587+MUTHURAJA1997@users.noreply.github.com>
2018-03-16 06:55:33 +0000

    Create append.c

---
+++ append.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int main()
+{
+    char *str = "blablabla";
+    char c = 'H';
+
+    size_t len = strlen(str);
+    char *str2 = malloc(len + 1 + 1 ); /* one for extra char, one for trailing zero */
+    strcpy(str2, str);
+    str2[len] = c;
+    str2[len + 1] = '\0';
+
+    printf( "%s\n", str2 ); /* prints "blablalaH" */
+
+    free( str2 );
+}
